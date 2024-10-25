3f2a9bfff50ff9a0a436d44d0fbc4d283829f343
Yana <cociconry@gmail.com>
2024-10-25 18:59:34 +0300

    ESHKERE

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Деев Леонид Русланович";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
